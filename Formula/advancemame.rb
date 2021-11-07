1374b41ea840ddc3caaba308aeb6077cd22e2b79
Stephen Schwetz <stephen@schwetz.com.au>
2021-11-07 19:17:33 +1100

    Revert "advancemame Correct Licence"
    This reverts commit d3e99b69bffd2f2960fa38756818c23d876e3c3f.

--- Formula/advancemame.rb
+++ Formula/advancemame.rb
@@ -3,8 +3,7 @@ class Advancemame < Formula
   homepage "https://www.advancemame.it/"
   url "https://github.com/amadvance/advancemame/releases/download/v3.9/advancemame-3.9.tar.gz"
   sha256 "3e4628e1577e70a1dbe104f17b1b746745b8eda80837f53fbf7b091c88be8c2b"
-  license "GPL-2.0-only"
-  revision 1
+  license "GPL-2.0"
 
   livecheck do
     url :stable
